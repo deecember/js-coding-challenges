33961f49da920dcb6bb5e0025ae128d03afe4fbb
verbxnnt-dev <jonasmilarg.catch@gmail.com>
2022-08-31 14:41:27 +0200

    feat(challenges): Added intermediate challenge 2 #0

---
+++ intermediate/2/challenge.js
@@ -0,0 +1,25 @@
+/*
+STEP 1
+
+DE: Schreibe eine Funktion, welche eine Zahl auf Oddish oder Evenish überprüft.
+	Folgendes soll in der Konsole ausgegeben werden:
+	● "The number 95782 is Oddish"
+	● "The number 95783 is Evenish"
+
+EN: Write a function that checks a number for Oddish or Evenish.
+	The following should be printed to the console:
+	● "The number 95782 is Oddish"
+	● "The number 95783 is Evenish"
+*/
+
+//--- Code goes here [Step 1] ---
+
+
+
+
+
+
+
+
+
+//--- Code goes here [Step 1] ---
---
+++ intermediate/2/solution.js
@@ -0,0 +1,25 @@
+/*
+STEP 1
+
+DE: Schreibe eine Funktion, welche eine Zahl auf Oddish oder Evenish überprüft.
+	Folgendes soll in der Konsole ausgegeben werden:
+	● "The number 95782 is Oddish"
+	● "The number 95783 is Evenish"
+
+EN: Write a function that checks a number for Oddish or Evenish.
+	The following should be printed to the console:
+	● "The number 95782 is Oddish"
+	● "The number 95783 is Evenish"
+*/
+
+//--- Code goes here [Step 1] ---
+function oddishOrEvenish(number) {
+	let result = 0;
+	String(number).split("").map((num) => {
+		result += parseInt(num);
+	})
+	return "The number "+ number +" is "+result%2 ? "Oddish" : "Evenish";
+}
+oddishOrEvenish(95782);
+oddishOrEvenish(95783);
+//--- Code goes here [Step 1] ---
